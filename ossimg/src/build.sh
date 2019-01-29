fa1d1e32da82b2bc660ebe6c2c4775a883574b38
wubingheng <wubinghengajw@outlook.com>
2019-01-29 10:56:18 +0800

    update ossimg build shell.

--- ossimg/src/build.sh
+++ ossimg/src/build.sh
@@ -3,3 +3,5 @@ DIR=$(cd ../; pwd)
 export GOPATH=$GOPATH:$DIR
 go build qufop.go
 mv qufop ../deploy/
+cp qufop.conf ../deploy/
+cp ../ossimg.conf ../deploy/
--- ossimg/src/cross_build.sh
+++ ossimg/src/cross_build.sh
@@ -3,3 +3,5 @@ DIR=$(cd ../; pwd)
 export GOPATH=$GOPATH:$DIR
 GOOS=linux GOARCH=amd64 go build qufop.go
 mv qufop ../deploy/
+cp qufop.conf ../deploy/
+cp ../ossimg.conf ../deploy/
